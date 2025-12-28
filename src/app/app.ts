7766a57c32b057042e4369d87fdb357d152fb462
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-12-27 23:58:21 -0500

    Setup app component with main application layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { Header } from './components/header/header';
+import { SideBar } from './components/side-bar/side-bar';
+import "ionicons"
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, SideBar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
