710107f81078b9bfe24c20df701b9ea69b966abd
Saminraiyan12 <samin.raiyan1@gmail.com>
2026-01-03 23:50:53 -0500

    Add error handling and functionality for signup page

--- src/app/pages/signup/signup.ts
+++ src/app/pages/signup/signup.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth-service/auth-service';
+import { InputError } from '../../directives/input-error';
 
 @Component({
   selector: 'app-signup',
-  imports: [RouterLink],
+  imports: [RouterLink, InputError],
   templateUrl: './signup.html',
   styleUrl: './signup.css',
 })
 export class Signup {
-
+  auth = inject(AuthService);
 }
