6f187aaab18b1113c717fd05ba8a48aa2de7d6ba
Saminraiyan12 <samin.raiyan1@gmail.com>
2026-01-03 23:50:23 -0500

    Remove ionicons import

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,6 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { SideBar } from './components/side-bar/side-bar';
-import "ionicons"
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, Header, SideBar],
