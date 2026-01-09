c5cb5d12831a61ba2e65f99f57923d545520a12b
Saminraiyan12 <samin.raiyan1@gmail.com>
2026-01-09 14:14:01 -0500

    Update auth styling and polish logic

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
-
+import { AuthService } from '../../services/auth-service/auth-service';
+import { InputError } from '../../directives/input-error';
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [RouterLink, InputError],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
-
+  auth = inject(AuthService)
 }
