8ad9523fadcfab567349e0d8ffd6766a4f15bdb7
Saminraiyan12 <samin.raiyan1@gmail.com>
2026-01-21 21:59:15 -0500

    Update login page pivoting to ReactiveFormsModule from pure signals

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -2,12 +2,35 @@ import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth-service/auth-service';
 import { InputError } from '../../directives/input-error';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
-  imports: [RouterLink, InputError],
+  imports: [RouterLink, InputError, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
-  auth = inject(AuthService)
+  private formBuilder = inject(FormBuilder);
+  loginForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.pattern(/^(?=.*\d).{8,}$/)]],
+  });
+  emailControlError = this.loginForm.get('email')?.errors;
+  get emailValid(){
+    const control = this.loginForm.get('email');
+    return !!(control?.invalid && control?.touched);
+  }
+  get passwordValid(){
+    const control = this.loginForm.get('password');
+    return !!(control?.invalid && control?.touched);
+  }
+
+  auth = inject(AuthService);
+  onSubmit() {
+    if (this.loginForm.invalid) return;
+    this.auth.login(
+      this.loginForm.get('email')?.value ?? '',
+      this.loginForm.get('password')?.value ?? ''
+    );
+  }
 }
--- src/app/services/auth-service/auth-service.ts
+++ src/app/services/auth-service/auth-service.ts
@@ -1,34 +1,13 @@
 import { computed, Injectable, signal } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  email = signal<string>('');
-  password = signal<string>('');
-  retypedPassword = signal<string>('');
-  private emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
-  private passwordRegex = /^(?=.*\d).{8,}$/;
-  isEmailValid = computed(() => this.emailRegex.test(this.email()));
-  isPasswordValid = computed(() => this.passwordRegex.test(this.password()));
-  isPasswordMatch = computed(() => this.password() === this.retypedPassword());
-  touched = signal<boolean>(false);
-  validateForm(type: 'login' | 'register'): boolean {
-    if (type === 'login') {
-      return this.isEmailValid() && this.isPasswordValid();
-    }
-    return this.isEmailValid() && this.isPasswordValid() && this.isPasswordMatch();
+  login(email: string, password: string) {
+    console.log(`Email: ${email}, Password: ${password}`);
   }
-  login() {
-    const isValid = this.validateForm('login');
-    console.log(`Email: ${this.email()}, Password: ${this.password()}, Valid: ${isValid}`);
-    this.touched.set(true);
-  }
-  register() {
-    const isValid = this.validateForm('register');
-    console.log(
-      `Email: ${this.email()}, Password: ${this.password()}, Retyped password: ${this.retypedPassword()}, Valid: ${isValid}`
-    );
-    this.touched.set(true);
+  register(email: string, password: string) {
+    console.log(`Email: ${email}, Password: ${password}}`);
   }
 }
